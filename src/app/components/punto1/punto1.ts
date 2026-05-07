69c7e018f864c6eea57c4269ac1d218e50e03f5d
Rodrigo Guanactolay <guanctolayrodrigo6@gmail.com>
2026-05-07 02:02:48 -0300

    Se implementa diseño al componente punto1 y se corrige direccion de imagenes de uso local.

--- src/app/components/punto1/punto1.ts
+++ src/app/components/punto1/punto1.ts
@@ -18,22 +18,22 @@ export class Punto1 {
     {
       nombre: 'Taller de analisis de datos con Python',
       descripcion: 'En este taller, aprenderás a analizar datos utilizando Python y sus bibliotecas populares como Pandas y Matplotlib.',
-      img: '../../assets/images/analisisDeDatos.jpg'
+      img: 'assets/images/analisisDeDatos.jpg'
     },
     {
       nombre: 'Charla sobre seguridad informática',
       descripcion: 'En esta charla, exploraremos los conceptos fundamentales de la seguridad informática y las mejores prácticas para proteger tus sistemas y datos.',
-      img: '../../assets/images/Seguridad-Informatica.jpg'
+      img: 'assets/images/Seguridad-Informatica.jpg'
     },
     {
       nombre: 'Masterclass de desarrollo web con Angular',
       descripcion: 'Aprende a construir aplicaciones web modernas con Angular, desde los conceptos básicos hasta técnicas avanzadas de desarrollo.',
-      img: '../../assets/images/masterClassAngular.jpg'
+      img: 'assets/images/masterClassAngular.jpg'
     },
     {
       nombre: 'Taller de inteligencia artificial',
       descripcion: 'En este taller, exploraremos los fundamentos de la inteligencia artificial y cómo aplicarla en proyectos prácticos utilizando herramientas como TensorFlow.',
-      img: '../../assets/images/ia.jpg'
+      img: 'assets/images/ia.jpg'
     }
   ]
   indiceActual: number = 0;
