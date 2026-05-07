a410fff4cb17bb1c59c5f474b5a9a6f01434e7ca
Rodrigo Guanactolay <guanctolayrodrigo6@gmail.com>
2026-05-07 01:26:33 -0300

    Se agrega logica para comportamiento del componente punto 1 en punto1.ts.

--- src/app/components/punto1/punto1.ts
+++ src/app/components/punto1/punto1.ts
@@ -1,9 +1,57 @@
 import { Component } from '@angular/core';
 
+interface Evento{
+  nombre: string;
+  descripcion: string;
+  img: string;
+}
 @Component({
   selector: 'app-punto1',
   imports: [],
   templateUrl: './punto1.html',
   styleUrl: './punto1.css',
 })
-export class Punto1 {}
+
+export class Punto1 {
+  //array de objetos
+  eventos: Evento[]=[
+    {
+      nombre: 'Taller de analisis de datos con Python',
+      descripcion: 'En este taller, aprenderás a analizar datos utilizando Python y sus bibliotecas populares como Pandas y Matplotlib.',
+      img: '../../assets/images/analisisDeDatos.jpg'
+    },
+    {
+      nombre: 'Charla sobre seguridad informática',
+      descripcion: 'En esta charla, exploraremos los conceptos fundamentales de la seguridad informática y las mejores prácticas para proteger tus sistemas y datos.',
+      img: '../../assets/images/Seguridad-Informatica.jpg'
+    },
+    {
+      nombre: 'Masterclass de desarrollo web con Angular',
+      descripcion: 'Aprende a construir aplicaciones web modernas con Angular, desde los conceptos básicos hasta técnicas avanzadas de desarrollo.',
+      img: '../../assets/images/masterClassAngular.jpg'
+    },
+    {
+      nombre: 'Taller de inteligencia artificial',
+      descripcion: 'En este taller, exploraremos los fundamentos de la inteligencia artificial y cómo aplicarla en proyectos prácticos utilizando herramientas como TensorFlow.',
+      img: '../../assets/images/ia.jpg'
+    }
+  ]
+  indiceActual: number = 0;
+
+  mostrarSiguienteEvento() {
+    if (this.indiceActual < this.eventos.length - 1) {
+      this.indiceActual++;
+    } else {
+      this.indiceActual = 0; //vuelve al primero
+    }
+  }
+
+  mostrarEventoAnterior() {
+    if (this.indiceActual > 0) {
+      this.indiceActual--;
+    } else {
+      this.indiceActual = this.eventos.length - 1; //va al último
+    }
+  }
+}
+
